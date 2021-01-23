f665f45d28c87def5776f130c682fa5c50c4ab0f
Calbabreaker <calbabreaker@gmail.com>
2021-01-23 17:57:56 +1030

    added minfied and module bundles

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,36 @@
 import typescript from "rollup-plugin-typescript2";
+import sourcemaps from "rollup-plugin-sourcemaps";
 import serve from "rollup-plugin-serve";
+import { terser } from "rollup-plugin-terser";
+
 import path from "path";
+import pkg from "./package.json";
 
 export default () => {
     const useDevServer = process.env.DEV_SERVER || false;
 
-    const browserBundle = {
+    const date = new Date().toUTCString();
+    const banner = [
+        `/*!`,
+        ` * ${pkg.name} - v${pkg.version}`,
+        ` * Compiled ${date}`,
+        ` *`,
+        ` * Free to use under the ${pkg.license} LICENSE.`,
+        ` */`,
+    ].join("\n");
+
+    const baseBundle = {
         input: path.join(__dirname, "src/index.ts"),
         output: {
+            sourcemap: true,
+            banner,
             name: "ESSEM",
-            file: path.join(__dirname, "build/essem.js"),
-            format: "umd",
         },
-        plugins: [typescript()],
+        plugins: [typescript(), sourcemaps()],
     };
 
     if (useDevServer) {
-        browserBundle.plugins.push(
+        baseBundle.plugins.push(
             serve({
                 open: true,
                 openPage: "/examples/",
@@ -25,5 +39,38 @@ export default () => {
         );
     }
 
-    return [browserBundle];
+    const bundles = [];
+
+    // unminified for browser
+    bundles.push({
+        ...baseBundle,
+        output: {
+            ...baseBundle.output,
+            file: path.join(__dirname, "build/essem.js"),
+            format: "umd",
+        },
+    });
+
+    // minified for browser
+    bundles.push({
+        ...baseBundle,
+        output: {
+            ...baseBundle.output,
+            file: path.join(__dirname, "build/essem.min.js"),
+            format: "umd",
+        },
+        plugins: [...baseBundle.plugins, terser()],
+    });
+
+    // for modules
+    bundles.push({
+        ...baseBundle,
+        output: {
+            ...baseBundle.output,
+            file: path.join(__dirname, "build/essem.module.js"),
+            format: "esm",
+        },
+    });
+
+    return bundles;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 export function test(): void {
     console.log("Hello testing yary");
 }
+
+declare global {
+    interface Window {
+        ESSEM: {};
+    }
+}
+
+if (typeof window !== "undefined") {
+    if (window.ESSEM != null) {
+        console.warn("ESSEM has been imported multiple times!");
+    }
+}
