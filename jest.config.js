6d35fc52c9e64d668004bf51f085f3431fa1a2db
Calbabreaker <57030377+Calbabreaker@users.noreply.github.com>
2021-04-19 15:42:43 +0930

    added basic website

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    preset: "ts-jest/presets/js-with-ts",
-    runner: "jest-electron/runner",
-    testEnvironment: "jest-electron/environment",
-    modulePaths: ["./"],
-};
--- src/utils/types.ts
+++
@@ -1,9 +0,0 @@
-export type AnyCtor<T> = new (...args: any[]) => T;
-export type ArrayCtor<T> = new (...items: T[]) => T[];
-
-declare global {
-    interface Window {
-        __ESSEM__: boolean;
-        ESSEM: { [key: string]: unknown };
-    }
-}
